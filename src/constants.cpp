019620fd820f87de27e39a3ac798992d9ed57d83
Antoine Qian <antoine.qian@c3.ai>
2022-10-21 10:59:27 +0200

    Clean up

--- src/constants.cpp
+++ src/constants.cpp
@@ -1,4 +1,5 @@
 #include "constants.hpp"
 
 const unordered_map<char, sf::Vector2<int>> constants::tilePositionsInMap = {{'p', {12, 2}},
-                                                                             {'b', {7, 6}}};
+                                                                             {'b', {7, 6}},
+                                                                             {'a', {2, 13}}};
