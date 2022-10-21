1c15adefe4332afd8b1d5e3ee31f7978f45cdb83
antoineqian <lieuxincertains@gmail.com>
2022-10-21 10:59:27 +0200

    Clean up

--- src/BlockTile.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Tile.hpp"
-
-class BlockTile : public Tile
-{
-public:
-	BlockTile(sf::Vector2f position, shared_ptr<TileDrawer> pDrawer);
-	void draw(sf::RenderWindow &window) const override;
-};
